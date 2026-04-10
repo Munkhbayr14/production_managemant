2a295e3088b277a49e8475d84438bc88b8699b68
Munkhbayar <munkhbayar.m@mongolcontent.mn>
2026-04-10 15:20:00 +0800

    fix: cleanup extra commented-out auth code (issue #1)

--- src/api/auth/auth.controller.ts
+++ src/api/auth/auth.controller.ts
@@ -110,11 +110,6 @@ export class AuthController {
     return this.authService.findOne(+id);
   }
 
-  // @Patch(':id')
-  // update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-  //   return this.authService.update(+id, updateAuthDto);
-  // }
-
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.authService.remove(+id);
